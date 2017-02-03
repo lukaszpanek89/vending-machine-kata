e9a9421d4ec708bb4d2ff47b4f34f6b2b402a210
Łukasz Panek <lukasz.panek@pentacomp.pl>
2017-02-03 15:54:07 +0100

    Refactorization: VendingMachineTests refactored to make use of mocked Shelves.

--- src/test/java/lpanek/tdd/unitTests/vendingMachine/VendingMachineTest.java
+++ src/test/java/lpanek/tdd/unitTests/vendingMachine/VendingMachineTest.java
@@ -2,81 +2,52 @@ package lpanek.tdd.unitTests.vendingMachine;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import lpanek.tdd.payment.Money;
-import lpanek.tdd.product.ProductType;
 import lpanek.tdd.vendingMachine.*;
 import lpanek.tdd.vendingMachine.ex.InvalidShelveNumberException;
 
+@RunWith(MockitoJUnitRunner.class)
 public class VendingMachineTest {
 
+    private static final String EXCEPTION_MESSAGE = "exception message";
+
     @Test
-    public void should_ThrowException_When_TriesToGetProductTypeForInvalidShelveNumber() {
+    public void should_ThrowException_When_ShelvesGetProductTypeOnShelveThrowsInvalidShelveNumberException() {
         // given
-        VendingMachine vendingMachine = createVendingMachineWithOneShelve();
+        Shelves shelvesMock = mock(Shelves.class);
+        doThrow(new InvalidShelveNumberException(EXCEPTION_MESSAGE)).when(shelvesMock).getProductTypeOnShelve(1);
+        VendingMachine vendingMachine = new VendingMachineBuilder().withShelves(shelvesMock).build();
 
         // when
-        Throwable caughtThrowable = catchThrowable(() -> vendingMachine.getProductTypeOnShelve(-1));
+        Throwable caughtThrowable = catchThrowable(() -> vendingMachine.getProductTypeOnShelve(1));
+
         // then
         assertThat(caughtThrowable)
                 .isNotNull()
                 .isInstanceOf(InvalidShelveNumberException.class)
-                .hasMessage("-1 is an invalid shelve number.");
-
-        // when
-        caughtThrowable = catchThrowable(() -> vendingMachine.getProductTypeOnShelve(0));
-        // then
-        assertThat(caughtThrowable)
-                .isNotNull()
-                .isInstanceOf(InvalidShelveNumberException.class)
-                .hasMessage("0 is an invalid shelve number.");
-
-        // when
-        caughtThrowable = catchThrowable(() -> vendingMachine.getProductTypeOnShelve(2));
-        // then
-        assertThat(caughtThrowable)
-                .isNotNull()
-                .isInstanceOf(InvalidShelveNumberException.class)
-                .hasMessage("2 is an invalid shelve number.");
+                .hasMessage(EXCEPTION_MESSAGE);
     }
 
     @Test
-    public void should_ThrowException_When_TriesToGetProductCountForInvalidShelveNumber() {
+    public void should_ThrowException_When_ShelvesGetProductCountOnShelveThrowsInvalidShelveNumberException() {
         // given
-        VendingMachine vendingMachine = createVendingMachineWithOneShelve();
+        Shelves shelvesMock = mock(Shelves.class);
+        doThrow(new InvalidShelveNumberException(EXCEPTION_MESSAGE)).when(shelvesMock).getProductCountOnShelve(1);
+        VendingMachine vendingMachine = new VendingMachineBuilder().withShelves(shelvesMock).build();
 
         // when
-        Throwable caughtThrowable = catchThrowable(() -> vendingMachine.getProductCountOnShelve(-1));
+        Throwable caughtThrowable = catchThrowable(() -> vendingMachine.getProductCountOnShelve(1));
+
         // then
         assertThat(caughtThrowable)
                 .isNotNull()
                 .isInstanceOf(InvalidShelveNumberException.class)
-                .hasMessage("-1 is an invalid shelve number.");
-
-        // when
-        caughtThrowable = catchThrowable(() -> vendingMachine.getProductCountOnShelve(0));
-        // then
-        assertThat(caughtThrowable)
-                .isNotNull()
-                .isInstanceOf(InvalidShelveNumberException.class)
-                .hasMessage("0 is an invalid shelve number.");
-
-        // when
-        caughtThrowable = catchThrowable(() -> vendingMachine.getProductCountOnShelve(2));
-        // then
-        assertThat(caughtThrowable)
-                .isNotNull()
-                .isInstanceOf(InvalidShelveNumberException.class)
-                .hasMessage("2 is an invalid shelve number.");
-    }
-
-    private VendingMachine createVendingMachineWithOneShelve() {
-        ProductType snacksType = new ProductType("Snacks", new Money(4));
-        Shelve shelve = new Shelve(snacksType, 7);
-        Shelves shelves = new Shelves();
-        shelves.add(shelve);
-        return new VendingMachineBuilder().withShelves(shelves).build();
+                .hasMessage(EXCEPTION_MESSAGE);
     }
 }
