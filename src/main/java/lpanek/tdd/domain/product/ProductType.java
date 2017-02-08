3ff1c1f1b45a5349d2447516e4a2bab8954d5325
Łukasz Panek <lukasz.panek@pentacomp.pl>
2017-02-08 17:55:24 +0100

    Added tests and implementation of ProductType.equals() and hashCode().

--- src/main/java/lpanek/tdd/domain/product/ProductType.java
+++ src/main/java/lpanek/tdd/domain/product/ProductType.java
@@ -2,12 +2,20 @@ package lpanek.tdd.domain.product;
 
 import lpanek.tdd.domain.payment.Money;
 
+import java.util.Objects;
+
 public class ProductType {
 
+    private String name;
     private Money price;
 
     public ProductType(String name, Money price) {
+        this.name = name;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Money getPrice() {
@@ -15,7 +23,27 @@ public class ProductType {
     }
 
     @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object.getClass() != this.getClass()) {
+            return false;
+        }
+
+        ProductType other = (ProductType) object;
+
+        return (other.name.equals(this.name))
+                && (other.price.equals(this.price));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    @Override
     public String toString() {
-        return String.format("%s=[%s]", getClass().getSimpleName(), price.toString());
+        return String.format("%s=[%s, %s]", getClass().getSimpleName(), name, price.toString());
     }
 }
