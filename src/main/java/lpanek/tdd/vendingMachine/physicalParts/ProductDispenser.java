05644a9ef078f3170a08d14725affd87880e8e4d
Łukasz Panek <lukasz.panek@pentacomp.pl>
2017-02-08 16:18:07 +0100

    Implementation of ProductDispenser methods extended to pass new tests.

--- src/main/java/lpanek/tdd/vendingMachine/physicalParts/ProductDispenser.java
+++ src/main/java/lpanek/tdd/vendingMachine/physicalParts/ProductDispenser.java
@@ -4,28 +4,40 @@ import java.util.LinkedList;
 import java.util.List;
 
 import lpanek.tdd.domain.product.Product;
+import lpanek.tdd.domain.product.ProductType;
 import lpanek.tdd.domain.shelves.Shelves;
 import lpanek.tdd.vendingMachine.physicalParts.ex.NoProductToTakeException;
 import lpanek.tdd.vendingMachine.physicalParts.listeners.ProductDispenserListener;
 
 public class ProductDispenser {
 
+    private Shelves shelves;
+    private Product dispensedProduct;
     private List<ProductDispenserListener> listeners = new LinkedList<>();
 
     public ProductDispenser(Shelves shelves) {
-
+        this.shelves = shelves;
     }
 
     public void dispenseProductFromShelve(int shelveNumber) {
         // Communication with product dispensing driver should happen here.
 
+        ProductType productType = shelves.getProductTypeOnShelve(shelveNumber);
+        dispensedProduct = new Product(productType);
+
         listeners.stream().forEach(ProductDispenserListener::onProductDispensed);
     }
 
     public Product takeProduct() throws NoProductToTakeException {
+        if (dispensedProduct == null) {
+            throw new NoProductToTakeException("There is no product to take.");
+        }
+
         listeners.stream().forEach(ProductDispenserListener::onProductTaken);
 
-        return null;
+        Product productToReturn = dispensedProduct;
+        dispensedProduct = null;
+        return productToReturn;
     }
 
     public void addListener(ProductDispenserListener listener) {
